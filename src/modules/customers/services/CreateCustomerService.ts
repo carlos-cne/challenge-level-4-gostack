4e95bc0c7eea1619a5d8625034faa58716bbf522
Carlos <carlos.queiroz-ext@ab-inbev.com>
2020-08-17 07:22:15 -0300

    finishing challenge

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const existingEmail = await this.customersRepository.findByEmail(email);
+
+    if (existingEmail) {
+      throw new AppError('Email already in use.');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
